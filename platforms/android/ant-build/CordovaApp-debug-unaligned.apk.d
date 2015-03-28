d082be7f9d8200fc10c590b04253f0c5f1af2fa1
Chrisotpher Oudyk <oudy0001@algonquinlive.com>
2015-03-28 15:45:00 -0400

    please work

--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
-C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\CordovaApp.ap_ \
-C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes.dex \
-C:\wamp\www\mad9022\midterm02\platforms\android\CordovaLib\ant-build\classes.jar \
