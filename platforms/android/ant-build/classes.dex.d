a270e40b56fb2cdfe882faa355c630b291a3db74
Chrisotpher Oudyk <oudy0001@algonquinlive.com>
2015-03-28 15:41:40 -0400

    having problems with get again

---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,18 @@
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes.dex : \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\ca\edumedia\oudy0001\midterm\BuildConfig.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\ca\edumedia\oudy0001\midterm\CordovaApp.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\ca\edumedia\oudy0001\midterm\R$attr.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\ca\edumedia\oudy0001\midterm\R$drawable.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\ca\edumedia\oudy0001\midterm\R$string.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\ca\edumedia\oudy0001\midterm\R$xml.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\ca\edumedia\oudy0001\midterm\R.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactAccessor$WhereOptions.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactAccessor.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactAccessorSdk5.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactInfoDTO.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager$1.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager$2.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager$3.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager$4.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager.class \
+C:\wamp\www\mad9022\midterm02\platforms\android\ant-build\dexedLibs\classes-a9a7cd9d42748b9fe85dc0ffe5e90294.jar \
